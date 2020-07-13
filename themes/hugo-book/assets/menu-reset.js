3d39b0191ab9b6fdee10ac52b22caef50027f1ed
wrajnees <watsh.rajneesh@oracle.com>
2020-07-13 17:27:44 -0500

    Fixed copyright issues reported in scan

--- themes/hugo-book/assets/menu-reset.js
+++ themes/hugo-book/assets/menu-reset.js
@@ -1,3 +1,5 @@
+// Copyright (c) 2020, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 (function() {
   var menu = document.querySelector("aside.book-menu nav");
   addEventListener("beforeunload", function(event) {
--- themes/hugo-book/assets/sw-register.js
+++ themes/hugo-book/assets/sw-register.js
@@ -1,3 +1,5 @@
+// Copyright (c) 2020, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register(
     "{{ "/sw.js" | relURL }}", 
