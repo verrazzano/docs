18a1cdfaa16991ef4a0eba3fb797d968d5b550c1
Guoyong Zhang <guoyong.zhang@oracle.com>
2021-07-26 16:09:24 -0400

    check quality

--- scripts/check.sh
+++ scripts/check.sh
@@ -4,9 +4,7 @@ set -e
 
 DOCS_DIR="content"
 
-ls $DOCS_DIR
-
-if grep -nrP -e "\(https://verrazzano.io/(?!v[0-9]\.[0-9]/|archive/)" .; then
+if grep -nrP -e "\(https://verrazzano.io/(?!v[0-9]\.[0-9]/|archive/)" ${DOCS_DIR}; then
   echo "Replace references to https://verrazzano.io wih relative references"
   exit 1
 fi
