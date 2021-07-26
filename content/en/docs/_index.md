8a3ecdbfcfe6c12f928357b97aa6e5c93edfc2aa
Guoyong Zhang <guoyong.zhang@oracle.com>
2021-07-26 11:56:16 -0400

    add versions

--- content/en/docs/_index.md
+++ content/en/docs/_index.md
@@ -1,6 +1,6 @@
 ---
 title: "Welcome to Verrazzano"
-linkTitle: "Documentation"
+linkTitle: "Documentation v1.0"
 weight: 20
 menu:
   main:
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,15 +8,22 @@ set -e
 
 BRANCH_NAME=$1
 
-if [[ ${BRANCH_NAME} == release-1.0 ]]; then
+LATEST_VERSION=$2
+
+if [[ ${BRANCH_NAME} == release-* ]]; then
   VERSION=v${BRANCH_NAME:8}
   sudo npm -g install gh-pages@3.0.0
   git config --global credential.helper "!f() { echo username=\\$GIT_AUTH_USR; echo password=\\$GIT_AUTH_PSW; }; f"
   git config --global user.name $GIT_AUTH_USR
   git config --global user.email "${EMAIL}"
   sudo chmod -R o+wx /usr/lib/node_modules
-  echo "publish ${VERSION}"
-  /usr/bin/gh-pages -d production -b gh-pages
+  if [[ "${VERSION}" == "${LATEST_VERSION}" ]]; then
+    echo "publish ${VERSION} as the latest"
+    /usr/bin/gh-pages -d production -b gh-pages -e latest
+  else
+    echo "publish ${VERSION}"
+    /usr/bin/gh-pages -d production -b gh-pages -e ${VERSION}
+  fi
 else
   echo "${BRANCH_NAME} is not a release branch.  Can not publish."
   exit 1
